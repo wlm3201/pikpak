954ea7bd28e1c7d7820e0b447c564200e218204d
wlm3201 <77601125+wlm3201@users.noreply.github.com>
2022-08-24 00:20:49 +0800

    Update list.vue

--- src/views/list.vue
+++ src/views/list.vue
@@ -775,7 +775,7 @@ import axios from 'axios';
           if(downFileList.value.length) {
             setTimeout(() => {
               postOne()
-            }, 100)
+            }, 300)
           } else {
             setTimeout(() => {
               nRef.value?.destroy()
